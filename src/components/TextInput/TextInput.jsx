49c79db33afa9077dc013342d26320ee4d85b9da
Oleh Usatyi <olegysik1@ukr.net>
2024-10-24 00:40:04 +0200

    refactor: update text input

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,17 +1,24 @@
 import styles from './TextInput.module.css';
+import sprite from '@assets/sprite.svg';
 
 const TextInput = ({ label, value, onChange, placeholder, id }) => {
   return (
     <div className={styles.inputContainer}>
       <label htmlFor={id} className={styles.label}>{label}</label>
-      <input
-        type="text"
-        id={id}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        className={styles.input}
-      />
+      <div className={styles.inputWrapper}>
+
+        <input
+          type="text"
+          id={id}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          className={styles.input}
+        />
+        <svg width="20" height="20" className={styles.inputIcon}>
+          <use xlinkHref={`${sprite}#icon-map`} />
+        </svg>
+      </div>
     </div>
   );
 };
