d868e6d2ebcd461b1250f0c0455e83c41f5bb2a7
Oleh Usatyi <olegysik1@ukr.net>
2024-10-25 22:09:07 +0200

    feat: add filter by location

--- src/components/LocationFilter/LocationFilter.jsx
+++ src/components/LocationFilter/LocationFilter.jsx
@@ -1,14 +1,27 @@
 import TextInput from '@components/TextInput/TextInput.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilters } from '@redux/filtersSlice.js';
 
 const LocationFilter = () => {
-  const handleChange = () => {
+  const dispatch = useDispatch();
+  const filters = useSelector((state) => state.filters);
+
+  const handleFilterChange = (e) => {
+    const { value } = e.target;
+    dispatch(setFilters({ location: value }));
   };
 
   return (
     <div>
-      <TextInput label={'Location'} onChange={handleChange} placeholder={'City'} id={'location'} />
+      <TextInput
+        label={'Location'}
+        value={filters.location}
+        onChange={handleFilterChange}
+        placeholder={'City'}
+        id={'location'} />
     </div>
   );
 };
+
 
 export default LocationFilter;
